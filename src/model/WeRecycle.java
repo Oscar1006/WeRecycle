1d519f0cd257c690e10c68f2e0cab6fc7f167b2f
Oscar1006 <oscar.suarez1@correo.icesi.edu.co>
2019-10-27 03:49:05 -0500

    Update WeRecycle.java

--- src/model/WeRecycle.java
+++ src/model/WeRecycle.java
@@ -14,7 +14,8 @@ public class WeRecycle {
 		products = new Product[1000];
 	}
 	
-	public String addWaste(String id, String name, String source, String color, int decompositionTime, Product product, String adviceToReduce) {
+	public String addWaste(String id, String name, String source, String color, int decompositionTime, 
+Product product, String adviceToReduce) {
 		String info = "";
 		boolean added = false;
 		Inert x = new Inert(id, name, source, color, decompositionTime, product, adviceToReduce);
@@ -34,8 +35,8 @@ public class WeRecycle {
 		return info;
 	}
 	
-	public String addWaste(String id, String name, String source, String color, int decompositionTime, Product product,
-			String type, String description) {
+	public String addWaste(String id, String name, String source, String color, int decompositionTime, 
+Product product, String type, String description) {
 		String info = "";
 		boolean added = false;
 		Recyclable x = new Recyclable(id, name, source, color, decompositionTime, product, type, description);
@@ -55,8 +56,8 @@ public class WeRecycle {
 		return info;
 	}
 	
-	public String addWaste(String id, String name, String source, String color, int decompositionTime, Product product,
-			boolean suitableForComposting) {
+	public String addWaste(String id, String name, String source, String color, int decompositionTime, 
+Product product, boolean suitableForComposting) {
 		String info = "";
 		boolean added = false;
 		Biodegradable x = new Biodegradable(id, name, source, color, decompositionTime, product, suitableForComposting);
@@ -95,6 +96,68 @@ public class WeRecycle {
 		
 		return info;
 	}
+	
+	public String generateReportOfWaste() {
+		String report = "";
+		
+		Inert x;
+		Recyclable y;
+		Biodegradable z;
+		
+		report += "INERT PRODUCTS \n ";
+		for(int i=0; i<waste.length; i++) {
+			if(waste[i] instanceof Inert) {
+				x = (Inert) waste[i];
+				report += "* \n" + x.toString();
+			}
+		}
+		
+		report += "RECYCLABLE PRODUCTS \n ";
+		for(int i=0; i<waste.length; i++) {
+			if(waste[i] instanceof Recyclable) {
+				y = (Recyclable) waste[i];
+				report += "* \n" + y.toString();
+			}
+		}
+		
+		report += "BIODEGRADABLE PRODUCTS \n ";
+		for(int i=0; i<waste.length; i++) {
+			if(waste[i] instanceof Biodegradable) {
+				z = (Biodegradable) waste[i];
+				report += "* \n" + z.toString();
+			}
+		}
+		
+		return report;
+	}
 
+	public String searchInfoWasteByName(String name) {
+		String info = "\n Not found \n";
+		
+		Inert x;
+		Recyclable y;
+		Biodegradable z;
+		
+		boolean stop = false;
+		
+		for (int i=0; !stop && i<waste.length;i++) {
+			if (waste[i].getName().equalsIgnoreCase(name)) {
+				if (waste[i] instanceof Inert) {
+					x = (Inert)waste[i];
+					info = "\n"+ x.toString() + "\n";
+				}
+				else if (waste[i] instanceof Recyclable) {
+					y = (Recyclable)waste[i];
+					info = "\n" + y.toString() + "\n";
+				}
+				else if (waste[i] instanceof Biodegradable) {
+					z = (Biodegradable)waste[i];
+					info = "\n" + z.toString() + "\n";
+				}
+				stop = true;
+			}	
+		}
+		return info;
+	}
 
 }
