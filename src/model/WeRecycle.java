8fbb281fab313621c5ff0176deeb9e0599debdab
Oscar1006 <oscar.suarez1@correo.icesi.edu.co>
2019-10-27 00:32:55 -0500

    Go

--- src/model/WeRecycle.java
+++ src/model/WeRecycle.java
@@ -5,5 +5,11 @@ public class WeRecycle {
 //RELATIONSHIPS
 	private Residue waste[];
 	private Product products[];
+	
+//CONSTRUCTOR
+	public WeRecycle(){
+		waste = new Residue[1000];
+		products = new Product[1000];
+	}
 
 }
