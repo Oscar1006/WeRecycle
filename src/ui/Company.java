68126e2df935c95a9d0b9c28249619fcd45761c1
Oscar1006 <oscar.suarez1@correo.icesi.edu.co>
2019-10-27 16:52:22 -0500

    Update Company.java

--- src/ui/Company.java
+++ src/ui/Company.java
@@ -23,6 +23,21 @@ public class Company {
 		Company Recycler = new Company();
 		int pick = 0;
 		boolean loop = true;
+		
+		System.out.println("		Menu\n"
+				+ " 1.Add residue\n"
+				+ " 2.Generate report about the registred waste\n"
+				+ " 3.Add product and the residues that may generate\n"
+				+ " 4.Search information of a residue with its name\n"
+				+ " 5.Search information of a residue with its id\n"
+				+ " 6.Show list of the resistred products\n"
+				+ " 7.Calculate harmful effect of a residue\n"
+				+ " 8.Determine if a biodegradable or recyclable residue is usable\n"
+				+ " 9.Ordenate the residues of a product by their nocive effect, showing the most nocive first");
+		
+		while(loop) {
+			
+		}
 
 	}
 
