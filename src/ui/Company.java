a08bcc0d702e533b6b843e0636ebd108eb52e031
Oscar1006 <oscar.suarez1@correo.icesi.edu.co>
2019-10-27 18:34:31 -0500

    Update Company.java

--- src/ui/Company.java
+++ src/ui/Company.java
@@ -1,5 +1,6 @@
 package ui;
 
+import model.Recyclable;
 import model.WeRecycle;
 import java.util.Scanner;
 
@@ -20,7 +21,7 @@ public class Company {
 	}
 	
 	public static void main(String[] args) {
-		Company Recycler = new Company();
+		Company recycler = new Company();
 		int pick = 0;
 		boolean loop = true;
 		
@@ -33,12 +34,147 @@ public class Company {
 				+ " 6.Show list of the resistred products\n"
 				+ " 7.Calculate harmful effect of a residue\n"
 				+ " 8.Determine if a biodegradable or recyclable residue is usable\n"
-				+ " 9.Ordenate the residues of a product by their nocive effect, showing the most nocive first");
+				+ " 9.Ordenate all the residues by their nocive effect, showing the most nocive first\n"
+				+ " 10.Ordenate the residues of a product by their nocive effect, showing the most nocive first\n"
+				+ " 11.Exit\n");
 		
 		while(loop) {
+			System.out.println("Please enter a number to choose an option of the menu\n");
+			pick = recycler.inputInt.nextInt();
+			
+			switch (pick){
+				case 1:
+						recycler.addResidue();
+					break;
+				case 2:
+						recycler.generateReport();
+					break;
+				case 3:
+						recycler.addProduct();
+					break;
+				case 4:
+						recycler.searchInfoByName();
+					break;
+				case 5:
+						recycler.searchInfoByName();	
+					break;
+				case 6:
+						recycler.showListOfProducts();
+					break;
+				case 7:
+						recycler.calculateHarmfulEffect();
+					break;
+				case 8:
+						recycler.detrmineUsability();
+					break;
+				case 9:
+						recycler.ordenateAllResiduesNoxiousness();
+					break;
+				case 10:
+						recycler.odenateResiduesOfAProductNoxiousness();
+					break;
+				case 11:
+					loop = false;
+					break;
+				default:
+					System.out.println("\n	Please enter a valid number\n");
+					
+			}
+		}
+	}
+
+	private void addResidue() {
+	System.out.println("Choose the category of the residue:\n "
+			+ "1.Inert\n"
+			+ "2.Recyclable\n"
+			+ "3.Biodegradable\n");	
+	int category = inputInt.nextInt();
+	System.out.println("Enter the id of the residue: ");
+	String id = inputString.nextLine();
+	System.out.println("Enter the name of the residue: ");
+	String name = inputString.nextLine();
+	System.out.println("Enter the source of the residue: ");
+	String source = inputString.nextLine();
+	System.out.println("Enter the color of the residue: ");
+	String color = inputString.nextLine();
+	System.out.println("Enter the decomposition time of the residue (days): ");
+	int decompositionTime = inputInt.nextInt();
+	
+	String adviceToReduce = "";
+	int nType = 0;
+	String type ="";
+	String description = "";
+	boolean suitableForComposting = false;
+	
+	switch (category) {
+		case 1:
+			System.out.println("Enter an advice to reduce this residue\n");
+			adviceToReduce = inputString.nextLine(); 
+				break;
+		case 2:
+			System.out.println("Choose its type:\n"
+					+ " 1.Paper\n"
+					+ " 2.Paperboard\n"
+					+ " 3.Glass\n"
+					+ "	4.Plastic\n"
+					+ " 5.Metal\n");
+			nType = inputInt.nextInt();
+			switch(nType) {
+				case 1:
+					type = Recyclable.PAPER;
+						break;
+				case 2:
+					type = Recyclable.PAPERBOARD;
+						break;
+				case 3:
+					type = Recyclable.GLASS;
+						break;
+				case 4:
+					type = Recyclable.PLASTIC;
+						break;
+				case 5: 
+					type = Recyclable.METAL;
+						break;
+			}
+			System.out.println("Enter a description of this residue\n");
+			description = inputString.nextLine();
+		case 3:
 			
 		}
+	}
+	
+	
 
+	private void generateReport() {
+			
+	}
+
+	private void addProduct() {
+			
+	}
+
+	private void searchInfoByName() {
+			
+	}
+
+	private void showListOfProducts() {
+			
+	}
+
+	private void calculateHarmfulEffect() {
+			
+	}
+
+	private void detrmineUsability() {
+			
+	}
+
+	private void ordenateAllResiduesNoxiousness() {
+			
+	}
+
+	private void odenateResiduesOfAProductNoxiousness() {
+			
 	}
 
 }
