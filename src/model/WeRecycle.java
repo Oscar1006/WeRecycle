032fa924826decec2e20b50b5d050304c294fc93
Oscar1006 <oscar.suarez1@correo.icesi.edu.co>
2019-10-27 01:07:32 -0500

    Go

--- src/model/WeRecycle.java
+++ src/model/WeRecycle.java
@@ -1,4 +1,6 @@
 package model;
+import model.Residue;
+import model.Product;
 
 public class WeRecycle {
 
@@ -11,5 +13,69 @@ public class WeRecycle {
 		waste = new Residue[1000];
 		products = new Product[1000];
 	}
+	
+	public String addWaste(String id, String name, String source, String color, int decompositionTime, Product product, String adviceToReduce) {
+		String info = "";
+		boolean added = false;
+		Inert x = new Inert(id, name, source, color, decompositionTime, product, adviceToReduce);
+		
+		for(int i=0; i<waste.length && !added; i++) {
+			if (waste[i] == null) {
+				waste[i] = x;
+				added = true;
+			}
+		}
+		
+		if (added)
+			info = "Residue added";
+		else
+			info = "Error";
+		
+		return info;
+	}
+	
+	public String addWaste(String id, String name, String source, String color, int decompositionTime, Product product,
+			String type, String description) {
+		String info = "";
+		boolean added = false;
+		Recyclable x = new Recyclable(id, name, source, color, decompositionTime, product, type, description);
+		
+		for(int i=0; i<waste.length && !added; i++) {
+			if (waste[i] == null) {
+				waste[i] = x;
+				added = true;
+			}
+		}
+		
+		if (added)
+			info = "Residue added";
+		else
+			info = "Error";
+		
+		return info;
+	}
+	
+	public String addWaste(String id, String name, String source, String color, int decompositionTime, Product product,
+			boolean suitableForComposting) {
+		String info = "";
+		boolean added = false;
+		Biodegradable x = new Biodegradable(id, name, source, color, decompositionTime, product, suitableForComposting);
+		
+		for(int i=0; i<waste.length && !added; i++) {
+			if (waste[i] == null) {
+				waste[i] = x;
+				added = true;
+			}
+		}
+		
+		if (added)
+			info = "Residue added";
+		else
+			info = "Error";
+		
+		return info;
+	}
+
+
 
 }
